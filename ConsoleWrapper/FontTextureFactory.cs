441d0aabd499915949849778cc3b04034fc46d3e
Tom <tom@tom.net.nz>
2009-10-05 03:05:49 +0000

    Started converting LineSprite to use individual letter sprites. Not stable.

---
+++ ConsoleWrapper/FontTextureFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace ConsoleWrapper
+{
+    class FontTextureFactory
+    {
+        private static IDictionary<string, FontTexture> _fontTextures;
+
+        public static FontTexture GetFontTexture(string fontFace, int fontSize, Device device)
+        {
+            if (_fontTextures == null)
+            {
+                _fontTextures = new Dictionary<string, FontTexture>();
+            }
+
+            string fontKey = fontFace + "_" + fontSize;
+
+            if (!_fontTextures.ContainsKey(fontKey))
+            {
+                _fontTextures.Add(fontKey, new FontTexture(fontFace, fontSize, device));
+            }
+
+            return _fontTextures[fontKey];
+        }
+
+        public static void Rebuild(Device device)
+        {
+            foreach (FontTexture font in _fontTextures.Values)
+            {
+                font.Rebuild(device);
+            }
+        }
+    }
+}
